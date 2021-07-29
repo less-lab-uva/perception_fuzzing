920cda1d7b782b15df3408c3d24a41ba6ace5a80
Trey Woodlief <adwiii7@gmail.com>
2021-07-29 17:39:32 -0400

    Added framework to store multiple SUTs

---
+++ src/sut_runner/sut_manager.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from src.images.image_mutator import MutationFolder
+from src.sut_runner.sut_runner import SUTRunner
+
+
+class SUTManager:
+    def __init__(self, suts: List[SUTRunner]):
+        self.suts = suts
+
+    def run_suts(self, mutation_folder: MutationFolder):
+        for sut in self.suts:
+            sut.run_semantic_seg(mutation_folder.folder, mutation_folder.get_sut_folder(sut.name))
--- src/sut_runner/sut_runner.py
+++ src/sut_runner/sut_runner.py
@@ -1,5 +1,18 @@
 import os
 
+
+def normalize_folders(folder, dest_folder=None):
+    if not folder[-1] == '/':  # normalize the folder_to_check name to have a trailing slash
+        folder += '/'
+    if dest_folder is None:
+        dest_folder = folder
+    elif not dest_folder[-1] == '/':
+        dest_folder += '/'
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+    return folder, dest_folder
+
+
 class SUTRunner:
     HOME_DIR = '/home/adwiii/'
     TEMP_DIR = HOME_DIR + 'tmp/'
@@ -8,24 +21,13 @@ class SUTRunner:
     POST_FIX = '_prediction.png'
 
     def __init__(self, sut_name):
-        self.sut_name = sut_name
-
-    def normalize_folders(self, folder, dest_folder=None):
-        if not folder[-1] == '/':  # normalize the folder_to_check name to have a trailing slash
-            folder += '/'
-        if dest_folder is None:
-            dest_folder = folder
-        elif not dest_folder[-1] == '/':
-            dest_folder += '/'
-        if not os.path.exists(dest_folder):
-            os.makedirs(dest_folder)
-        return folder, dest_folder
+        self.name = sut_name
 
     def _run_semantic_seg(self, folder, dest_folder):
         """Run the SUT on the specified input folder, then copy the results to the dest folder"""
         pass
 
     def run_semantic_seg(self, folder, dest_folder=None):
-        folder, dest_folder = self.normalize_folders(folder, dest_folder)
+        folder, dest_folder = normalize_folders(folder, dest_folder)
         self._run_semantic_seg(folder, dest_folder)
 
